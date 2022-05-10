9240e94f1c2e6a2aeebd7b2ba7f28bcffc14f120
RomanKhrapchun <khrapchun@gmail.com>
2022-05-10 20:39:42 +0300

    first ProductController changes

--- models/Products.php
+++ models/Products.php
@@ -6,7 +6,43 @@ use yii\db\ActiveRecord;
 
 class Products extends ActiveRecord {
 
+public $_product;
+public $_characteristic;
+public $_productname;
+public $_cost;
+public $_productphoto;
 
+public static function tableName()
+    {
+        return 'products';
+    }
 
+    public static function findIdentity($id)
+    {
+        return static::findOne($id);
+    }
 
+    public static function findByProductname($productname)
+    {
+        $product = static::find()->where(['name'=>$productname])->one();
+        if ($product) {
+            $product->_id = $product->id;
+            $product->_name = $product->name;
+            $product->_productname = $product->productname;
+            return $product;
+        } 
+        return null;
+    }
+
+    public static function findById($id)
+    {
+        $product = static::find()->where(['id'=>$id])->one();
+        if ($product) {
+            $product->_id = $product->id;
+            $product->_name = $product->name;
+            $product->_productname = $product->productname;
+            return $product;
+        } 
+        return null;
+    }
 }
